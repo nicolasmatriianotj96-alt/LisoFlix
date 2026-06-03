373b37a393675c1acf98614e99834b6bd617e53e
Nicolas <nicolasmatriiano@gmail.com>
2026-06-03 15:22:51 -0300

    fix ssl2

--- BackEnd/db.js
+++ BackEnd/db.js
@@ -3,37 +3,12 @@ require('dotenv').config();
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false }
+    ssl: { rejectUnauthorized: false },
+    family: 4
 });
 
-pool.query('SELECT 1').then(() => console.log('DB conectado')).catch(e => console.log('Erro no banco:', e.message));
+pool.query('SELECT 1')
+    .then(() => console.log('DB conectado'))
+    .catch(e => console.log('Erro no banco:', e.message));
 
 module.exports = pool;
-
-require("dotenv").config();
-
-const db = new Pool({
-
-    connectionString: process.env.DATABASE_URL,
-
-    ssl: {
-        rejectUnauthorized: false
-    },
-
-    family: 4
-
-});
-
-db.connect()
-    .then(() => {
-
-        console.log("Supabase conectado");
-
-    })
-    .catch((err) => {
-
-        console.log("Erro no banco:", err);
-
-    });
-
-module.exports = db;
