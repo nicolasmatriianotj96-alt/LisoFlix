911827ce3243db989d62a2bd47fcbb65751967ea
Nicolas <nicolasmatriiano@gmail.com>
2026-06-03 13:50:27 -0300

    correcao

--- BackEnd/db.js
+++ BackEnd/db.js
@@ -1,13 +1,8 @@
 const { Pool } = require("pg");
-
-console.log("DATABASE_URL lida pelo db.js:", process.env.DATABASE_URL)
-
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+  ssl: { rejectUnauthorized: false }
 })
-
-
 
 module.exports = pool
 
